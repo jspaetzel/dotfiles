20578a611dd18ecab996e689e9c3f7cf1a04cda9
John Spaetzel <john.spaetzel@hellosign.com>
2018-12-12 15:19:03 -0800

    add vscode settings

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# Basic dotfiles
 FILES=( .bash_profile .profile .gitconfig )
 for i in ${FILES[@]}; do
   ln -sf $(pwd)/$i $HOME/$i
 done
+
+# VS Code User Settings
+ln -sf $(pwd)/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
